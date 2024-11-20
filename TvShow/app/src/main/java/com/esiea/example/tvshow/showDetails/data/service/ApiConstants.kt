acd02e640bdd84fb940ecfc99e29cd96a2431280
Namory <nnamorykanate5@gmail.com>
2024-11-20 12:02:02 +0100

    first commit ShowDetails

---
+++ TvShow/app/src/main/java/com/esiea/example/tvshow/showDetails/data/service/ApiConstants.kt
@@ -0,0 +1,7 @@
+package com.esiea.example.tvshow.showDetails.data.service
+
+object ApiConstants {
+    const val BASE_URL = "https://www.episodate.com/api/"
+    const val MOST_POPULAR = "most-popular"
+    const val PAGE = 1
+}
