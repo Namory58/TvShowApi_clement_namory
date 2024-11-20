bd7b7a17dd2b26562320ace119f9b66656933914
Namory <nnamorykanate5@gmail.com>
2024-11-20 11:57:02 +0100

    config service Api

--- TvShow/app/src/main/java/com/esiea/example/tvshow/mostPopular/data/service/NewApiService.kt
+++ TvShow/app/src/main/java/com/esiea/example/tvshow/mostPopular/data/service/NewApiService.kt
@@ -1,4 +1,13 @@
 package com.esiea.example.tvshow.mostPopular.data.service
 
+import com.esiea.example.tvshow.mostPopular.data.dto.ReponseDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NewApiService {
+    @GET(ApiConstants.MOST_POPULAR)
+    suspend fun getMostPopularTvShows(
+        @Query("page")
+        page: Int = ApiConstants.PAGE
+    ): ReponseDto
 }
